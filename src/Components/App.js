c313bf8e9fe1f713be1d6a3ef5e8e7a7f9e8771d
Pooja Choudhary <poojachoudhary560@gmail.com>
2019-02-10 23:15:22 +0530

    Added Tab on change and list

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,47 @@
 import React, { Component, Fragment } from "react";
 import { Header, Footer } from "./Layouts";
 import Exercises from "./Exercises";
+import { muscles, exercises } from "../store.js";
 export default class extends Component {
+  state = {
+    exercises,
+    category: "shoulders"
+  };
+  getExercisesByMuscles() {
+    return Object.entries(
+      this.state.exercises.reduce((exercises, exercise) => {
+        const { muscles } = exercise;
+
+        exercises[muscles] = exercises[muscles]
+          ? [...exercises[muscles], exercise]
+          : [exercise];
+
+        return exercises;
+      }, {})
+    );
+  }
+  handleCategorySelected = category => {
+    this.setState(
+      {
+        category
+      },
+      () => console.log("category selected " + this.state.category)
+    );
+  };
   render() {
+    console.log(this.getExercisesByMuscles());
+    const exercises = this.getExercisesByMuscles(),
+      { category } = this.state;
     return (
       <Fragment>
         <Header />
 
-        <Exercises />
-        <Footer />
+        <Exercises exercises={exercises} />
+        <Footer
+          category={category}
+          muscles={muscles}
+          onSelect={this.handleCategorySelected}
+        />
       </Fragment>
     );
   }
--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -2,12 +2,31 @@ import React from "react";
 import { Paper, Tabs } from "@material-ui/core";
 import Tab from "@material-ui/core/Tab";
 
-export default props => (
-  <Paper>
-    <Tabs indicatorColor="primary" value={0} textColor="primary" centered>
-      <Tab label="Item One" />
-      <Tab label="Item Two" />
-      <Tab label="Item Three" />
-    </Tabs>
-  </Paper>
-);
+export default ({ muscles, category, onSelect }) => {
+  console.log("category " + category);
+  const index = category
+    ? muscles.findIndex(group => group === category) + 1
+    : 0;
+
+  const onIndexSelect = (e, index) => {
+    console.log("index " + index);
+    console.log("muscles[index - 1] " + muscles[index - 1]);
+    onSelect(index === 0 ? "" : muscles[index - 1]);
+  };
+  return (
+    <Paper>
+      <Tabs
+        indicatorColor="primary"
+        value={index}
+        onChange={onIndexSelect}
+        textColor="primary"
+        centered
+      >
+        <Tab label="All" />
+        {muscles.map(group => (
+          <Tab label={group} />
+        ))}
+      </Tabs>
+    </Paper>
+  );
+};
